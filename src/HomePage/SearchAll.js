a2dbd2ed19dbf2efe04fac229d4301ad8df63abe
tahsinUchiha <alton255@yahoo.co.uk>
2018-11-23 13:30:37 +0000

    updates on homepage

---
+++ src/HomePage/SearchAll.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './SearchAll.css'
+
+
+
+class SearchAll extends React.Component {
+  render() {
+
+    return (
+
+      <form>
+        <input className="search" type="text"
+
+          placeholder="    Search Dinosaur" />
+
+        <input className="button" type="button" value="Search"/>
+
+       
+      </form>
+
+    );
+
+  }
+
+}
+
+export default SearchAll;
