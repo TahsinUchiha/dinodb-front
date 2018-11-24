ce3b9e8f3344cd38f204101521510a2bbc53d240
TahsinUchiha <alton255@yahoo.co.uk>
2018-11-24 12:24:12 +0000

    home edits

---
+++ src/HomePage/Home.js
@@ -0,0 +1,29 @@
+// import React, { Component } from 'react';
+// import SearchAll from './SearchAll.js';
+// import './Home.css'
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
+// class Home extends React.Component {
+//   render() {
+
+//     return (
+//       <div>
+//         <ul className="homebar">
+//         <h1 className="title"><a>Dino-DataBase</a></h1>
+//         <li><a>All Dinosaur</a></li>
+//         <li><a>Register</a></li>
+//         <li><a>Add</a></li>
+//         <li><a>Login</a></li>
+//         <li><a>About</a></li>
+//         <li className="searchbar" style={{float:"right"}}><SearchAll /></li>
+//         </ul> 
+//       </div>
+
+//     );
+
+//   }
+
+// }
+
+// export default Home;
